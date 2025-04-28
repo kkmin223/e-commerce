3e4b9c905bf498e0948a4736eb8f8c40f343588d
Kang Min <rkdals133@gmail.com>
2025-04-28 23:17:58 +0900

    feat : 인기 판매 조회를 위한 캐싱 적용

--- src/main/java/kr/hhplus/be/server/ServerApplication.java
+++ src/main/java/kr/hhplus/be/server/ServerApplication.java
@@ -8,8 +8,8 @@ import org.springframework.retry.annotation.EnableRetry;
 @SpringBootApplication
 public class ServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
 
 }
